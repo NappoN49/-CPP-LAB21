d7c71b5858b4a15d9ff9504b51d2dc566a9325a1
NappoN49 <parawin39228@gmail.com>
2026-02-20 14:50:16 +0700

    average complete

--- average.cpp
+++ average.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+#include <string>
 
+using namespace std;
+
+int main(int argc,char *argv[]){
+	if(argc==1){
+		cout<<"Please input numbers to find average.";
+	}
+	else{
+		double sumw=0;
+		for(int i=1;i<argc;i++) sumw+=stod(argv[i]);
+		cout<<"---------------------------------\n";
+		cout<<"Average of "<<argc-1<<" numbers = "<< sumw/(argc-1.0) <<"\n";
+		cout<<"---------------------------------";
+	}
+    return 0;
+}
